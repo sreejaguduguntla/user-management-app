c05b0ae799c1b4dbf69973cc7aa660b9ec7729ac
Sreeja Guduguntla <sreejaguduguntla@gmail.com>
2026-05-13 11:52:20 +0530

    changes in UserList.jsx, added render url

--- Frontend/src/components/UserList.jsx
+++ Frontend/src/components/UserList.jsx
@@ -8,7 +8,7 @@ function UsersList() {
   useEffect(() => {
     async function getUsers() {
       try {
-        let res = await fetch("http://localhost:4000/user-api/users", {
+        let res = await fetch("https://user-management-app-xl5k.onrender.com/user-api/users", {
           method: "GET",
         });
 
